1f19820b40967dcc2d3793999794a2a0fccf697e
VictorSu000 <287764747@qq.com>
2017-09-17 21:53:43 +0800

    Complete app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import settings
+import time
+import os
 from flask import Flask, render_template, \
     session, redirect, url_for, request, abort
 from functools import wraps
@@ -30,13 +32,14 @@ def main():
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
-    if request.methods == 'GET':
+    if request.method == 'GET':
         return render_template('login.html')
-    elif request.methods == 'POST':
+    elif request.method == 'POST':
         username = request.form.get('username', '')
         password = request.form.get('password', '')
         if username == settings.USERNAME and password == settings.PASSWORD:
             session['loggedin'] = True
+            return redirect(url_for('manage'))
         else:
             abort(401)
     else:
@@ -47,6 +50,28 @@ def login():
 @login_required
 def manage():
     return render_template('manage.html')
+
+
+@app.route('/migrate/', methods=['POST'])
+@login_required
+def migrate():
+    if request.method == 'POST':
+        js = request.form.get('migrationsjs', '')
+        if js == '':
+            abort(400)
+        else:
+            jsFileName = time.strftime(
+                '%Y-%m-%d_%H:%M:%S', time.localtime(time.time())) +\
+                '_ip:' + request.remote_addr
+            jsFileFullPath = settings.MIGARTEJSSCRIPTBASEPATH + jsFileName
+            with open(jsFileFullPath, 'w') as jsFile:
+                jsFile.write(js)
+            result = os.popen('source ' + settings.MIGRATEBASHSCRIPTPATH +
+                              ' ' + settings.BACKUPBASEPATH +
+                              ' ' + jsFileName)
+        return result.read()
+    else:
+        abort(405)
 
 
 if __name__ == '__main__':
